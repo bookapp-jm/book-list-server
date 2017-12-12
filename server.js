3ae2c121b49b4c6d1a7bf4feada99bc9c625e8de
Mitchell M <mitchellm1618@gmail.com>
2017-12-12 10:34:47 -0800

    got server.js up and running, populating database and table properly

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,7 +17,9 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+
+
 
 
 
@@ -24,3 +27,43 @@ app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 // CLIENT_URL = http://localhost:8080
 // DATABASE_URL = postgres://localhost:5432/
+
+function loadBooks() {
+  client.query('SELECT COUNT(*) FROM books')
+    .then(result => {
+      if (!parseInt(result.rows[0].count)) {
+        fs.readFile('../book-list-client/data/books.json', (err, fd) => {
+          JSON.parse(fd.toString()).forEach(ele => {
+            client.query(`
+              INSERT INTO
+              books(title, author, isbn, image_url, description)
+              VALUES ($1, $2, $3, $4, $5);
+            `,
+              [ele.title, ele.author, ele.isbn, ele.image_url, ele.description]
+            );
+          });
+        });
+      }
+    });
+}
+
+function loadDB() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS books (
+      book_id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      author VARCHAR(255) NOT NULL,
+      isbn VARCHAR (255) NOT NULL,
+      image_url VARCHAR(255) NOT NULL,
+      description TEXT NOT NULL); 
+    `)
+    .then(loadBooks)
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+
+loadDB();
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
